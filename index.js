bf090295bf3d6f65bbb4743aceae379f150a0437
Stephen <127278005+slmov215@users.noreply.github.com>
2023-05-11 23:53:20 -0400

    added required files and worked on prompt messages

---
+++ index.js
@@ -0,0 +1,82 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require("./utils/generateMarkdown")
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title of the project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Briefly, describe the project',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'How do you install your project?',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'How do you use this project?',
+    name: 'usage', 
+  },
+  {
+    type: 'input',
+    message: 'List the contributors, if any',
+    name: 'contribution', 
+  },
+  {
+    type: 'input',
+    message: 'How do you run a test on this project?',
+    name: 'test', 
+  },
+  {
+    type: 'list',
+    message: 'Choose a license for this project',
+    name: 'license', 
+    choices: [
+      'MIT', 'Apache', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None',
+    ]
+  },
+  {
+    type: 'input',
+    message: 'Enter you GitHub Username',
+    name: 'github', 
+  },
+  {
+    type: 'input',
+    message: 'Enter your email',
+    name: 'email', 
+  },
+];
+  
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  let myReadMe = generateMarkdown(data)
+  fs.writeFile(fileName, myReadMe, (err) =>
+    err ? console.error(err) : console.log('Success')
+  );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions)
+  .then((answers) => {
+    console.log(answers)
+    writeToFile('README.md', answers)
+  })
+  .catch((error) => {
+    if (error.isTtyError){
+      //prompt couldn't be rendered in the current environment
+    } else {
+      //something went wrong
+    }
+  })
+}
+
+// Function call to initialize app
+init();
